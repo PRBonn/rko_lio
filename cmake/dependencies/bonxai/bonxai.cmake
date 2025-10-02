10292a183f9c4d2979aa350a94d543f906fc911e
Meher Malladi <rm.meher97@gmail.com>
2025-10-02 16:48:19 +0200

    update bonxai version

--- cmake/dependencies/bonxai/bonxai.cmake
+++ cmake/dependencies/bonxai/bonxai.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
   Bonxai
   GIT_REPOSITORY https://github.com/facontidavide/Bonxai.git
-  GIT_TAG 47ffd0a2917c899f6199dfa71445481164298006 # apr 7 2025 master
+  GIT_TAG 02d401b1ce38bce870c6704bcd4e35a56a641411 # sep 14 2025 master
   SOURCE_SUBDIR
   bonxai_core
   SYSTEM
