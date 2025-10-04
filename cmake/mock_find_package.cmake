f5b14cbfddebd6b079bb3cc8af8883c6f503489c
Meher Malladi <rm.meher97@gmail.com>
2025-10-04 18:37:06 +0200

    add license

--- cmake/mock_find_package.cmake
+++ cmake/mock_find_package.cmake
@@ -1,3 +1,23 @@
+# Copyright (c) 2025 Meher V.R. Malladi.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 macro(mock_find_package_for_older_cmake PACKAGE_NAME)
   # Only if CMake < 3.24 (no OVERRIDE_FIND_PACKAGE)
   if(NOT CMAKE_VERSION VERSION_GREATER_EQUAL "3.24")
