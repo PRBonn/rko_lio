8ef58f2f67f6f2e423035b68550e1e8b48eda6fc
Meher Malladi <rm.meher97@gmail.com>
2025-10-21 15:11:03 +0200

    reorganize tests a bit

--- python/tests/test_imports.py
+++ python/tests/test_imports.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import pytest
 
 
@@ -13,35 +11,3 @@ def test_rko_lio_pybind_import():
     from rko_lio import rko_lio_pybind
 
     assert rko_lio_pybind is not None
-
-
-def test_helipr_pybind_import():
-    from rko_lio.dataloaders import helipr_file_reader_pybind
-
-    assert hasattr(helipr_file_reader_pybind, "read_lidar_bin")
-
-
-def test_helipr_import():
-    from rko_lio.dataloaders.helipr import HeliprDataLoader
-
-    assert HeliprDataLoader is not None
-
-
-def test_rosbag_import():
-    _ = pytest.importorskip(
-        "rosbags",
-        reason="Optional dependency for rosbag dataloader (rosbags) not installed",
-    )
-    from rko_lio.dataloaders.rosbag import RosbagDataLoader
-
-    assert RosbagDataLoader is not None
-
-
-def test_raw_import():
-    _ = pytest.importorskip(
-        "open3d",
-        reason="Optional dependency for raw dataloader (open3d) not installed",
-    )
-    from rko_lio.dataloaders.raw import RawDataLoader
-
-    assert RawDataLoader is not None
