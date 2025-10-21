9d146478423542d23a121b7bd0b1f4346f875a1f
Meher Malladi <rm.meher97@gmail.com>
2025-10-21 13:16:42 +0200

    add optional dataloader tests, and add a workflow to install and test

--- python/tests/test_imports.py
+++ python/tests/test_imports.py
@@ -19,3 +19,32 @@ def test_helipr_pybind_import():
     from rko_lio.dataloaders import helipr_file_reader_pybind
 
     assert hasattr(helipr_file_reader_pybind, "read_lidar_bin")
+
+
+def test_helipr_import():
+    from rko_lio.dataloaders.helipr import HeliprDataloader
+
+    assert HeliprDataloader is not None
+
+
+@pytest.mark.skipif(
+    not pytest.importorskip(
+        "open3d", reason="Optional dependency for raw dataloader (open3d) not installed"
+    ),
+)
+def test_raw_import():
+    from rko_lio.dataloaders.raw import RawDataLoader
+
+    assert RawDataLoader is not None
+
+
+@pytest.mark.skipif(
+    not pytest.importorskip(
+        "rosbags",
+        reason="Optional dependency for rosbag dataloader (rosbags) not installed",
+    ),
+)
+def test_rosbag_import():
+    from rko_lio.dataloaders.rosbag import RosbagDataLoader
+
+    assert RosbagDataLoader is not None
